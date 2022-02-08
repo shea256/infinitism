f84da26fb482ac3ebef36a86651d6d5b459eb19b
Ryan Shea <ryan@shea.io>
2022-02-08 12:43:28 -0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '👋',
+  footerText: <>{new Date().getFullYear()} © Ryan Shea.</>,
+  unstable_faviconGlyph: '♾️',
 }
